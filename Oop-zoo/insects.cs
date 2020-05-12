41f895c1662b04ba598e6e03c5b2697be10ff3c1
sihem azibi <s.azibi@live.fr>
2020-05-11 22:16:32 -0500

    insect lass ans mamels done

---
+++ Oop-zoo/insects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop_zoo
+{
+    public abstract class Insects : Animals
+    {
+
+        public override int Age => 1;
+        public override decimal Weight => 10m;
+
+        public override int Id => 07;
+
+        public abstract string Wings { get; }
+
+
+        public virtual bool fly()
+        {
+            Console.WriteLine("can all the insects fly!!");
+            return false;
+
+        }
+    }
+}
---
+++ Oop-zoo/mamels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop_zoo
+{
+    abstract class mamels:Animals
+    {
+        public override int Age => 7;
+        public override decimal Weight => 100m;
+
+        public override int Id => 08;
+
+        public abstract string Hair { get; }
+
+
+        public virtual bool Nurse()
+        {
+            Console.WriteLine("does All Mamels nurse! ");
+            return false;
+        }
+    }
+}
