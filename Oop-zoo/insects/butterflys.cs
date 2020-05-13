f988bdac624ed4f152996eae52195bf7a6cee29b
sihem azibi <s.azibi@live.fr>
2020-05-12 23:45:53 -0500

    first submission

--- Oop-zoo/insects/butterflys.cs
+++ Oop-zoo/insects/butterflys.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace Oop_zoo
 {
-    class butterflys
+    public class butterflys:Insects
     {
+        public override int Age => 1;
+        public override decimal Weight => 1m;
+
+        public override int Id => 0654657;
+
+        public override string Wings => "four";
+
+
+        public override bool fly()
+        {
+            Console.WriteLine("butterflys fly!!");
+            return true;
+
+        }
+        public override string Sound()
+        {
+            return "the butterflys frrr";
+        }
+
+        public override string Sleep()
+        {
+            return "the betterfly sleep ";
+        }
+        public override string Eat()
+        {
+            return "betterflys eat from the flowers";
+        }
     }
 }
--- Oop-zoo/insects/flys.cs
+++ Oop-zoo/insects/flys.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace Oop_zoo
 {
-    class flys
+   public class flys:Insects
     {
+        public override int Age => 1;
+        public override decimal Weight => 1m;
+
+        public override int Id => 065589657;
+
+        public override string Wings => "two";
+
+
+        public override bool fly()
+        {
+            Console.WriteLine("flys fly!!");
+            return true;
+
+        }
+        public override string Sound()
+        {
+            return "the flys zzzzz";
+        }
+
+        public override string Sleep()
+        {
+            return "the flys sleep ";
+        }
+        public override string Eat()
+        {
+            return "flys eat trash ";
+        }
     }
 }
